89ba27fdeabec4e467a41d017761dc688262df9e
InakiSebastianDesktop <inakicabrerao@gmail.com>
2024-11-01 12:03:46 +0100

    FEATURE: AÑADIDO JS PARA OCULTAR MENU BASICO Y MOSTRAR MENU CIUDADES

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,17 +1,22 @@
-jQuery(document).ready(function()
-{
-    var contenedorGps = $("#cajaBtnGps");
-    var contenedorBuscar = $("#cajaBtnBuscar");
-    var botonGps = $("#btnGps");
-    var botonBuscar = $("#btnBuscar");
-    var cajaInputCiudad = $("#cajaInputCiudad");
-    cajaInputCiudad.hide()
+jQuery(document).ready(function () {
+  var contenedorGps = $("#cajaBtnGps");
+  var contenedorBuscar = $("#cajaBtnBuscar");
+  var botonGps = $("#btnGps");
+  var botonBuscar = $("#btnBuscar");
+  var cajaInputCiudad = $("#cajaInputCiudad");
+  var movimientoOBusqueda = false;
+  cajaInputCiudad.hide();
 
-
-    botonBuscar.on("click", function ()  {
-        contenedorGps.hide();
-        cajaInputCiudad.show();
-    });
-
-
+  botonBuscar.on("click", function () {
+    if (movimientoOBusqueda) {
+    } else {
+      //  contenedorGps.hide();
+      contenedorGps.removeClass("col-lg-6");
+      contenedorBuscar.removeClass("col-lg-6");
+      contenedorGps.addClass("col-lg-3");
+      contenedorBuscar.removeAttr("col-lg-3");
+      cajaInputCiudad.show();
+      movimientoOBusqueda = true;
+    }
+  });
 });
