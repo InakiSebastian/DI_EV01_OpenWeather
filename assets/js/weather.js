92174451441482a0f7fa71c284b31f1f54176465
InakiSebastianDesktop <inakicabrerao@gmail.com>
2024-11-01 13:04:13 +0100

    FEATURE: AÑADIDO BOTON VOLVER CIUDAD

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -3,9 +3,22 @@ jQuery(document).ready(function () {
   var contenedorBuscar = $("#cajaBtnBuscar");
   var botonGps = $("#btnGps");
   var botonBuscar = $("#btnBuscar");
+  var botonCerrar = $("#btnClose");
   var cajaInputCiudad = $("#cajaInputCiudad");
+  var cajaBtnClose = $("#cajaBtnClose");
   var movimientoOBusqueda = false;
   cajaInputCiudad.hide();
+  cajaBtnClose.hide();
+
+ botonCerrar.on("click", function () {
+    contenedorGps.addClass("col-lg-6");
+    contenedorBuscar.addClass("col-lg-6");
+    contenedorGps.removeClass("col-lg-2");
+    contenedorBuscar.removeClass("col-lg-2");
+    cajaBtnClose.hide();
+    cajaInputCiudad.hide();
+    movimientoOBusqueda = false;
+  });
 
   botonBuscar.on("click", function () {
     if (movimientoOBusqueda) {
@@ -13,10 +26,13 @@ jQuery(document).ready(function () {
       //  contenedorGps.hide();
       contenedorGps.removeClass("col-lg-6");
       contenedorBuscar.removeClass("col-lg-6");
-      contenedorGps.addClass("col-lg-3");
-      contenedorBuscar.removeAttr("col-lg-3");
+      contenedorGps.addClass("col-lg-2");
+      contenedorBuscar.addClass("col-lg-2");
       cajaInputCiudad.show();
+      cajaBtnClose.show();
+
       movimientoOBusqueda = true;
     }
   });
 });
+
