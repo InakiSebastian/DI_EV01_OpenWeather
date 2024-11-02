1034aa12537741844316544464d646ad8a8b10c4
InakiSebastianDesktop <inakicabrerao@gmail.com>
2024-11-02 01:33:06 +0100

    FEATURE: AÑADIDO FUNCION POR GPS

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -9,12 +9,17 @@ jQuery(document).ready(function () {
   var cajaBtnClose = $("#cajaBtnClose");
   var cajaActual = $("#actual");
   const API_KEY = "32703d44a00c89012175a9bb40b70da7";
+  var datosTiempoActual = {};
+
   var movimientoOBusqueda = false;
   cajaInputCiudad.hide();
   cajaBtnClose.hide();
   cajaActual.hide();
+  var lat;
+  var lon;
 
   botonCerrar.on("click", function () {
+    inputCiudad.val("");
     contenedorGps.addClass("col-lg-6");
     contenedorBuscar.addClass("col-lg-6");
     contenedorGps.removeClass("col-lg-2");
@@ -25,16 +30,69 @@ jQuery(document).ready(function () {
     cajaActual.hide();
   });
 
+  $("#btnGps").on("click", function () {
+    inputCiudad.val("");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        busquedaPorCoordenadas();
+        cajaActual.show();
+        contenedorGps.removeClass("col-lg-6");
+        contenedorBuscar.removeClass("col-lg-6");
+        contenedorGps.addClass("col-lg-2");
+        contenedorBuscar.addClass("col-lg-2");
+        cajaBtnClose.show();
+  
+      });
+    } else {
+      alert("HAY PROBLEMAS CON LA GEOLOCALIZACIÓN. INTENTALO MAS TARDE");
+    }
+  });
+
+  function añadirTituloNombre(name){
+    $("#tituloActual").append("<h4>TIEMPO EN " + name + "</h4>");
+
+  }
+
+  function obtenerNombreCiudad() {
+    var url2 =
+      "https://api.openweathermap.org/geo/1.0/reverse?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=" +
+      API_KEY;
+
+    $.ajax({
+      // la URL para la petición
+      url: url2,
+
+      type: "GET",
+      // el tipo de información que se espera de respuesta
+      dataType: "json",
+      // código a ejecutar si la petición es satisfactoria;
+      // la respuesta es pasada como argumento a la función
+      success: function (json3) {
+        añadirTituloNombre(json3[0]["name"].toUpperCase());
+      },
+
+      // código a ejecutar si la petición falla;
+      error: function (xhr, status) {
+        alert("Disculpe, existió un problema");
+      },
+    });
+  }
+
   botonBuscar.on("click", function () {
+
     if (movimientoOBusqueda) {
       if (inputCiudad.val() != "") {
         consultaActual();
         cajaActual.show();
       } else {
         alert("RELLENA LA CIUDAD ANTES DE HACER UNA BUSQUEDA");
-       
       }
-
     } else {
       contenedorGps.removeClass("col-lg-6");
       contenedorBuscar.removeClass("col-lg-6");
@@ -47,33 +105,68 @@ jQuery(document).ready(function () {
     }
   });
 
+  
+
   function anadirDatosActualesAWeb(datos) {
     $("#loadingGif1").hide();
-
     $("#tituloActual").empty();
-    $("#tituloActual").append(
-      "<h4>TIEMPO EN " + inputCiudad.val().toUpperCase() + "</h4>"
-    );
-    console.log(datos);
-    var temperatura = Math.round(datos["tiempo"] - 273.15);
-    var iconoUrl = "https://openweathermap.org/img/wn/"+ datos["icon"]+"@2x.png"
+    if (inputCiudad.val() == "") {
+      
+      obtenerNombreCiudad();
+    } else {
+      $("#tituloActual").append("<h4>TIEMPO EN " + inputCiudad.val().toUpperCase() + "</h4>");
 
-    $("#iconoActual").empty()
-    $("#iconoActual").append($("<img  src='"+ iconoUrl +"'></img>"));
+    }
+    var temperatura = Math.round(datos["tiempo"] - 273.15);
+    var iconoUrl =
+      "https://openweathermap.org/img/wn/" + datos["icon"] + "@2x.png";
+
+    $("#iconoActual").empty();
+    $("#iconoActual").append($("<img  src='" + iconoUrl + "'></img>"));
 
     $("#temperaturaActual").empty();
     $("#temperaturaActual").append($("<h4>" + temperatura + "°C</h4>"));
   }
 
+  function busquedaPorCoordenadas() {
+    var url2 =
+      "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=" +
+      API_KEY;
+
+    $.ajax({
+      // la URL para la petición
+      url: url2,
+
+      type: "GET",
+      // el tipo de información que se espera de respuesta
+      dataType: "json",
+      // código a ejecutar si la petición es satisfactoria;
+      // la respuesta es pasada como argumento a la función
+      success: function (json2) {
+        datosTiempoActual["icon"] = json2.weather[0].icon;
+        datosTiempoActual["tiempo"] = json2.main.temp;
+        anadirDatosActualesAWeb(datosTiempoActual);
+        inputCiudad.val("");
+      },
+
+      // código a ejecutar si la petición falla;
+      error: function (xhr, status) {
+        alert("Disculpe, existió un problema");
+      },
+    });
+  }
+
   function consultaActual() {
-    var datosTiempoActual = {};
     var url1 =
       "https://api.openweathermap.org/geo/1.0/direct?q=" +
       inputCiudad.val() +
       "&limit=1&appid=" +
       API_KEY;
-    var lat;
-    var lon;
+
     $.ajax({
       // la URL para la petición
       url: url1,
@@ -91,35 +184,7 @@ jQuery(document).ready(function () {
           json.forEach((ciudad) => {
             lat = ciudad["lat"];
             lon = ciudad["lon"];
-            var url2 =
-              "https://api.openweathermap.org/data/2.5/weather?lat=" +
-              lat +
-              "&lon=" +
-              lon +
-              "&appid=" +
-              API_KEY;
-
-            $.ajax({
-              // la URL para la petición
-              url: url2,
-
-              type: "GET",
-              // el tipo de información que se espera de respuesta
-              dataType: "json",
-              // código a ejecutar si la petición es satisfactoria;
-              // la respuesta es pasada como argumento a la función
-              success: function (json2) {
-
-                datosTiempoActual["icon"] = json2.weather[0].icon;
-                datosTiempoActual["tiempo"]= json2.main.temp ;
-                anadirDatosActualesAWeb(datosTiempoActual);
-              },
-
-              // código a ejecutar si la petición falla;
-              error: function (xhr, status) {
-                alert("Disculpe, existió un problema");
-              },
-            });
+            busquedaPorCoordenadas();
           });
         }
       },
